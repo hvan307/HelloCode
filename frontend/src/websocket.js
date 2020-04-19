d4dfb63ab8a7232401a64d62882733d19131d34d
benharris8 <b.harris2657@gmail.com>
2020-04-19 14:18:46 +0100

    run serve front/backend working and local chat function working

--- frontend/src/websocket.js
+++ frontend/src/websocket.js
@@ -14,7 +14,7 @@ class WebSocketService {
   }
 
   connect(chatUrl) {
-    const path = `ws://127.0.0.1:4000/ws/chat/${chatUrl}/`;
+    const path = `ws://127.0.0.1:4000/ws/chat/${chatUrl}`;
     console.log(`Chat URL ${path}`)
     this.socketRef = new WebSocket(path);
     this.socketRef.onopen = () => {
