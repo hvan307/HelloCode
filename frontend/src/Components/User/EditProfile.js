70143f6cc5380d9f35684893870162bbf96bf95d
IAmNini <fortuny.carrega@gmail.com>
2020-04-22 17:47:59 +0100

    profile edit work in progress

--- frontend/src/Components/User/EditProfile.js
+++ frontend/src/Components/User/EditProfile.js
@@ -1,16 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { withRouter } from 'react-router-dom'
 import axios from 'axios'
+import auth from '../../lib/auth'
+
 import { makeStyles } from '@material-ui/core/styles'
 import Input from '@material-ui/core/Input'
-import InputAdornment from '@material-ui/core/InputAdornment'
 import Grid from '@material-ui/core/Grid'
-import EmailRounded from '@material-ui/icons/EmailRounded'
 import Face from '@material-ui/icons/Face'
-import Visibility from '@material-ui/icons/Visibility'
-import VisibilityOff from '@material-ui/icons/VisibilityOff'
-import LockRoundedIcon from '@material-ui/icons/LockRounded'
-import IconButton from '@material-ui/core/IconButton'
 import LanguageRoundedIcon from '@material-ui/icons/LanguageRounded'
 import ComputerRoundedIcon from '@material-ui/icons/ComputerRounded'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
@@ -20,18 +15,18 @@ import PhotoLibraryRoundedIcon from '@material-ui/icons/PhotoLibraryRounded'
 let selectedLangs = []
 let displayLangs = []
 
-const Register = (props) => {
+const EditProfile = (props) => {
 
-  const [data, setData] = useState({ email: '', username: '', password: '', password_confirmation: '', timezone: '', languages: [], image: '' })
-  // , error: '' 
-  const [password, setPassword] = useState({ showPassword: false, showPassConfirm: false })
+  const [data, setData] = useState({ image: '', username: '', timezone: '', languages: [] })
   const [langData, setLangData] = useState({ languagesDb: [] })
+
+  const id = auth.getUserId()
 
   useEffect(() => {
     console.log('hello')
     axios.get('/api/languages/')
       .then(resp => setLangData({ languagesDb: resp.data }))
-    // .catch(err => setData({ error: err.response.data }))
+      // .catch(err => setData({ error: err.response.data }))
   }, [])
 
   const handleSubmit = (event) => {
@@ -40,38 +35,20 @@ const Register = (props) => {
     // console.log(imageInput)
     const image = imageInput.files
     const form = new FormData()
-    console.log(image[0])
-    form.append('username', data.username)
-    form.append('email', data.email)
-    form.append('password', data.password)
-    form.append('password_confirmation', data.password_confirmation)
-    form.append('timezone', data.timezone)
-    data.languages.forEach(language => {
-      form.append('languages', language)
-    })
     form.append('image', image[0], image[0].name)
-    axios.post('/api/auth/register/', form)
+    form.append('username', data.username)
+    form.append('timezone', data.timezone)
+    form.append('languages', data.languages)
+
+    axios.put(`/api/user/${id}`, form)
       .then(resp => {
         setData({ data: resp.data })
-        props.history.push('/login')
+        props.history.push('/myprofile')
       })
-    // .catch(err => setData({ error: err.response.data }))
   }
 
   const handleChange = (prop) => (event) => {
     setData({ ...data, [prop]: event.target.value })
-  }
-
-  const handleClickShowPassword = () => {
-    setPassword({ ...password, showPassword: !password.showPassword })
-  }
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault()
-  }
-
-  const handleClickShowPassConfirm = () => {
-    setPassword({ ...password, showPassConfirm: !password.showPassConfirm })
   }
 
   const handleSelectLang = (event) => {
@@ -108,7 +85,7 @@ const Register = (props) => {
   }))
 
   const classes = useStyles()
-  
+
   const inputEl = useRef(null)
   const onButtonClick = () => {
     inputEl.current.click()
@@ -116,7 +93,7 @@ const Register = (props) => {
 
   return <>
     <div className='section register'>
-      <h2>Register</h2>
+      <h2>Edit Profile</h2>
       <form
         className='classes.form'
         onSubmit={() => handleSubmit(event)}
@@ -143,84 +120,6 @@ const Register = (props) => {
         <div className={classes.margin}>
           <Grid container spacing={1} alignItems='flex-end'>
             <Grid item>
-              <EmailRounded />
-            </Grid>
-            <Grid item>
-              <Input
-                className={classes.inputField}
-                type='text'
-                value={data.email}
-                onChange={handleChange('email')}
-                placeholder='E-mail'
-              />
-            </Grid>
-          </Grid>
-          {data.error && <FormHelperText error>
-            {data.error.email}
-          </FormHelperText>}
-        </div>
-        <div className={classes.margin}>
-          <Grid container spacing={1} alignItems='flex-end'>
-            <Grid item>
-              <LockRoundedIcon />
-            </Grid>
-            <Grid item>
-              <Input
-                className='classes.input'
-                type={password.showPassword ? 'text' : 'password'}
-                value={password.password}
-                onChange={handleChange('password')}
-                placeholder='Password'
-                endAdornment={
-                  <InputAdornment position='end'>
-                    <IconButton
-                      aria-label='toggle password visibility'
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {password.showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </Grid>
-          </Grid>
-          {data.error && <FormHelperText error>
-            {data.error.password[0]}
-          </FormHelperText>}
-        </div>
-        <div className={classes.margin}>
-          <Grid container spacing={1} alignItems='flex-end'>
-            <Grid item>
-              <LockRoundedIcon />
-            </Grid>
-            <Grid item>
-              <Input
-                type={password.showPassConfirm ? 'text' : 'password'}
-                value={data.password_confirmation}
-                onChange={handleChange('password_confirmation')}
-                placeholder='Confirm password'
-                endAdornment={
-                  <InputAdornment position='end'>
-                    <IconButton
-                      aria-label='toggle passConfirm visibility'
-                      onClick={handleClickShowPassConfirm}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {password.showPassConfirm ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </Grid>
-          </Grid>
-          {data.error && <FormHelperText error>
-            {data.error.password_confirmation[0]}
-          </FormHelperText>}
-        </div>
-        <div className={classes.margin}>
-          <Grid container spacing={1} alignItems='flex-end'>
-            <Grid item>
               <LanguageRoundedIcon />
             </Grid>
             <Grid item>
@@ -229,7 +128,7 @@ const Register = (props) => {
                 type='text'
                 value={data.timezone}
                 onChange={handleChange('timezone')}
-                placeholder='Your timezone (GMT)' />
+                placeholder='Your timezone' />
             </Grid>
           </Grid>
           {data.error && <FormHelperText error>
@@ -283,10 +182,7 @@ const Register = (props) => {
       </form>
     </div>
   </>
+
 }
-export default withRouter(Register)
 
-
-
-
-
+export default EditProfile
