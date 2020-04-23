31ffa65b4596e328e365f0fd027722793297d84f
benharris8 <b.harris2657@gmail.com>
2020-04-23 12:13:15 +0100

    ready to deploy

--- project/routing.py
+++ project/routing.py
@@ -1,6 +1,9 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
