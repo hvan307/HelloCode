01b3c510b095f0c239d356eb7ad38e61960ca13c
benharris8 <b.harris2657@gmail.com>
2020-04-23 16:57:31 +0100

    ready for deployment

--- project/asgi.py
+++ project/asgi.py
@@ -13,6 +13,5 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-os.environ['ASGI_THREADS']="4"
 django.setup()
 application = get_default_application()
