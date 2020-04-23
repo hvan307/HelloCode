aee3a066847090d95b75aed8a46381ed3d240c92
Alexandra <a.galitzine@gmail.com>
2020-04-23 16:11:06 +0100

    styling

--- frontend/src/Components/Common/NavbarDrawer.js
+++ frontend/src/Components/Common/NavbarDrawer.js
@@ -33,7 +33,7 @@ import HomeRoundedIcon from '@material-ui/icons/HomeRounded'
 
 
 const DrawerMenu = () => {
-  const drawerWidth = '100vw'
+  const drawerWidth = 240
   // styling - move to scss
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -172,14 +172,6 @@ const DrawerMenu = () => {
           paper: classes.drawerPaper
         }}
       >
-<<<<<<< HEAD
-        <Typography
-          variant='h6'
-          id='drawer-title'
-        >
-          Welcome, {<span className='username'> {auth.getUserName()} </span>} !
-            </Typography>
-=======
         {isLoggedIn &&
           <Typography
             variant='h6'
@@ -187,7 +179,6 @@ const DrawerMenu = () => {
           >
             Welcome, {<span className='username'> {auth.getUserName()} </span>} !
           </Typography>}
->>>>>>> development
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
             {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
