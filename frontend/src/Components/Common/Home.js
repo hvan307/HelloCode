4fba32448607392131d69ea76fa561ccc34d1852
IAmNini <fortuny.carrega@gmail.com>
2020-04-23 11:33:37 +0100

    styling

--- frontend/src/Components/Common/Home.js
+++ frontend/src/Components/Common/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
   return <>
     <div className="home-main-container">
       <div className="home-main">
-        <h1>Welcome to HelloCode!</h1>
+        <h1>Welcome to <span className="home-hello-code">HelloCode!</span></h1>
         <h2>Join millions of coders helping each other!</h2>
         <h3>We'll match you with someone so you can start chatting!</h3>
         <Link to={'/login'}>
